7cd1bb29ff3b3d7522304451495680525fdfd30c
codelzn <lzn724958732@gmail.com>
2022-12-07 11:45:58 +0900

    init-tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+	srcDir: 'src',
+	modules: [
+		'@pinia/nuxt',
+		'@nuxtjs/apollo',
+		'@vueuse/nuxt',
+		'@nuxtjs/tailwindcss',
+	],
+  tailwindcss: {
+    configPath: '~~/tailwind.config.js',
+		viewer: false,
+	},
+	apollo: {
+		clients: {
+			default: {
+				httpEndpoint:
+					'https://api-ap-northeast-1.hygraph.com/v2/clbcz9cnp1cl701t97a9z3lxm/master',
+			},
+		},
+	},
+	typescript: {
+		tsConfig: {
+			compilerOptions: {
+				strict: true,
+			},
+		},
+	},
 })
---
+++ src/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-red-500 text-8xl">Hello</h1>
+  </div>
+</template>
